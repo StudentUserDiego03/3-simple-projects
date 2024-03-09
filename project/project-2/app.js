a5fe46291c500ba7818339c5fc1c0821b689df7d
StudentUserDiego03 <dcardenascastane@student.alamo.edu>
2024-03-08 22:27:39 -0600

    tried to make projects original

--- project/project-2/app.js
+++ project/project-2/app.js
@@ -29,11 +29,35 @@ let data = [
     }
 ];
 
+function register(){    
+var nameInput = document.getElementById("input-name").value;
+var ageInput = document.getElementById("input-age").value;
+
+
+
+data.push(nameInput,ageInput);
+
+
+
+
+
 const info = document.querySelector("#info");
 let details = data.map(function(item){
+   
 
-    return "<div>" + item.name + " is " + item.age + " years old" + "</div>";
+    nameInput= data.name;
+    ageInput= data.age;
     
-})
+    return "<div>" + item.name + " is " + item.age + " years old" + "</div>";
+
+
+        
+}
+)
+
+
 
 info.innerHTML = details.join("\n");
+
+ console.log(info);
+}
